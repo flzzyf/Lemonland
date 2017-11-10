f888a11192d2ae42bb9d5bbd80fe83df46867dba
flzzyf <2981019122@qq.com>
2017-11-10 11:27:25 +0800

    3d

--- Lemonland/Assets/Script/Player.cs
+++ Lemonland/Assets/Script/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : Unit {
 
+    public GameObject flashLight;
+
+    public float rotSpeed = 3f;
+
 	void Start ()
     {
         Init();
@@ -32,6 +36,21 @@ public class Player : Unit {
             transform.Translate(X * speed * Time.deltaTime,
                                 Y * speed * Time.deltaTime,
                                 0, Space.World);
+        }
+
+        //鼠标世界点
+        Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 dir = new Vector3(mousePoint.x, mousePoint.y, 0) - transform.position;
+
+        //旋转
+        if (dir != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, rotSpeed * Time.deltaTime);
+
         }
     }
 }
